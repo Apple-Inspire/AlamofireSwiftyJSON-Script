10bbc07d2023bc00468b0532bdec3be36385999b
Yash Nayak <yugn27@gmail.com>
2018-09-08 16:36:09 +0530

    Create AS.sh

---
+++ AS.sh
@@ -0,0 +1,30 @@
+echo Installing AlamoFire and SwiftyJSON
+read -p "enter name of file> " fpath
+cd $fpath
+#ls
+
+sudo gem install cocoapods
+
+pod init
+
+read -p "enter your project folder name> " fname
+
+cat <<EOD > Podfile
+#isohelp
+platform :ios, '9.0'
+
+target '$fname' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+    pod 'Alamofire'
+    pod 'SwiftyJSON'
+  # Pods for Clima
+ 
+
+end
+
+EOD
+
+pod install
+
+echo "Done Installing you may open $fname .workspace file "
